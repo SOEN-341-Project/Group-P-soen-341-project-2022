ca73c1d1509e9bf7b8a3c623391ede19de904768
Adamo Orsini <adamo.orsini12@gmail.com>
2022-02-08 22:10:32 -0500

    Added comment, removed unnecessary import

--- online-shopping-website/src/Components/ProductGrid.js
+++ online-shopping-website/src/Components/ProductGrid.js
@@ -1,5 +1,4 @@
 import Card from '@mui/material/Card';
-import Button from '@mui/material/Button';
 import ButtonBase from '@mui/material/ButtonBase';
 import Grid from '@mui/material/Grid';
 import React from 'react';
