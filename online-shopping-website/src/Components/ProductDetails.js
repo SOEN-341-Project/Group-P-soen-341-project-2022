f6d07fbb597212abd03948a736f71de8ea9a6ef8
Alexandra <aspyridakos@gmail.com>
2022-02-10 12:06:33 -0500

    Fixed grid compilation error.
    Run time error of not rendering still exists.

--- online-shopping-website/src/Components/ProductDetails.js
+++ online-shopping-website/src/Components/ProductDetails.js
@@ -4,24 +4,24 @@ import Card from '@mui/material/Card';
 export const ProductDetails = (props) => {
     return props.products.map(product => {
         return (
-            <Grid item key={product.id} xs={12} md={6} lg={4}>
-                <ProductInformation product={product}/>
-            </Grid>
+            <ProductInformation product={product}/>
         );
     });
 }
 
 const ProductInformation = (props) => {
     return (
-        <Grid container xs={12} sm={9}>
-            <img src={props.product.image} alt={props.product.name}/>
-            <p>{props.product.description}</p>
-        </Grid>
-        <Grid container xs={12} sm={3}>
-            <Card>
-                <button>Add to cart</button>
-                <p>Quantity:</p>
-            </Card>
+        <Grid container>
+            <Grid item xs={12} sm={9}>
+                <img src={props.product.image} alt={props.product.name}/>
+                <p>{props.product.description}</p>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+                <Card>
+                    <button>Add to cart</button>
+                    <p>Quantity:</p>
+                </Card>
+            </Grid>
         </Grid>
     );
 }
