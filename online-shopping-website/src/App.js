5af4a599cdc5947b33f7b01842a517989d73fded
Alexandra Spyridakos <71853151+aspyridakos@users.noreply.github.com>
2022-02-14 15:42:43 -0500

    routing added

--- online-shopping-website/src/App.js
+++ online-shopping-website/src/App.js
@@ -1,24 +1,48 @@
 import './App.css';
-import { ProductPage } from './Components/ProductPage';
-import { SearchBar } from './Components/SearchBar';
-import { useState } from 'react';
+import {ProductPage} from './Components/ProductPage';
+import {SearchBar} from './Components/SearchBar';
+import {useState} from 'react';
 import Products from './TestValues.json';
+import {ProductPreview} from './Components/ProductGrid';
+import ReactDOM from "react-dom";
+import {BrowserRouter, Outlet, Link, Routes, Route} from "react-router-dom";
 
-function App() {
+const Layout = () => {
+    return (
+        <>
+            <div className="AppContent">
+                <h1>Bobble</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/searchbar">Search bar</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet/>
+        </>
+    )
+};
+
+export default function App() {
     // Products that will render
     const [products, setProducts] = useState(Products.products);
-    
+
     // Filters
     let [filters] = useState(
-        { 
-          searchQuery: '',
-          brands: [], 
-          sellers: [], 
-          lowestPrice: null, 
-          highestPrice: null
+        {
+            searchQuery: '',
+            brands: [],
+            sellers: [],
+            lowestPrice: null,
+            highestPrice: null
         }
     );
-    
+
     const filterProducts = () => {
         // Use RegEx for search query
         /*
@@ -26,7 +50,7 @@ function App() {
             i = Case insensitive
         */
         const query = new RegExp('^' + filters.searchQuery, 'i');
-        
+
         // Filter products according to search query, are within price range, and don't belong to brands or sellers arrays
         setProducts(Products.products.filter(product => (
             (product.name.split(' ').some(word => word.search(query) > -1) || product.name.search(query) > -1)
@@ -35,17 +59,28 @@ function App() {
             && !filters.brands.includes(product.brand)
             && !filters.sellers.includes(product.seller)
         )));
-    }    
+    }
 
     return (
-        <div className="AppContent">
-            <h1>
-                Bobble
-            </h1>
-            <SearchBar filterProducts={filterProducts} filters={filters} />
-            <ProductPage filterProducts={filterProducts} filters={filters} products={products} />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<ProductPage filterProducts={filterProducts} filters={filters}
+                           products={products}/>}/>
+                    <Route path="/searchbar" element={<SearchBar filterProducts={filterProducts} filters={filters}/>}/>
+                    <Route path="/:productName" element={<SamplePage/>}/>
+                    <Route path="*" element={<NoPage/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
-export default App;
+
+const SamplePage = () => {
+    return <h1>Sample Page</h1>;
+}
+
+const NoPage = () => {
+    return <h1>404</h1>;
+};
