2eea6bc0fa75cfa32a251b8627eb0f18d9cbf1a8
Jungsoo <jungsoo.lee4@gmail.com>
2022-02-18 15:05:23 -0500

    Added Stack to RegisterPage

--- online-shopping-website/src/Components/RegisterPage.js
+++ online-shopping-website/src/Components/RegisterPage.js
@@ -1,4 +1,15 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
 
+export const Register = () => {
+    return (
+        <div Class>
+            <Stack spacing={2}>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            </Stack>
+        </div>
+    )
+}
