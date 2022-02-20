0ad642cb4173022d2197f760dad56ad450756c8a
Alexandra <aspyridakos@gmail.com>
2022-02-19 21:47:41 -0500

    started selected product fetching from grid

--- online-shopping-website/src/Components/Seller/SellerProductsPage.js
+++ online-shopping-website/src/Components/Seller/SellerProductsPage.js
@@ -1,43 +1,29 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import ButtonBase from '@mui/material/ButtonBase';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import {Link} from "react-router-dom";
-import Products from '../../TestValues.json';
-
-import Sellers from '../../TestValues.json';
-// import {products, sellers} from '../../TestValues.json';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {DataGrid} from '@mui/x-data-grid';
+import { Link } from "react-router-dom";
+import { DataGrid} from '@mui/x-data-grid';
 import Button from '@mui/material/Button';
-
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import AddIcon from '@mui/icons-material/Add';
-
+import Products from '../../TestValues.json';
+import Sellers from '../../TestValues.json';
 
 const columns = [
-    {field: 'id', headerName: 'ID', type: 'number', width: 20, align: 'center',},
+    { field: 'id', headerName: 'ID', type: 'number', width: 20, align: 'center', },
     {
         field: 'image',
         headerName: 'Image',
         align: 'center',
         width: 70,
         renderCell: (params) => (
-            <img className="sellerImage" src={params.value}/>
+            <img className="sellerImage" src={params.value} alt="n/a" />
         ),
         sortable: false,
         filterable: false
     },
-    {field: 'name', headerName: 'Product name', width: 130},
-    {field: 'description', headerName: 'Description', width: 200},
+    { field: 'name', headerName: 'Product name', width: 130 },
+    { field: 'description', headerName: 'Description', width: 200 },
     {
         field: 'price', headerName: 'Price', type: 'number', width: 90,
         valueGetter: (params) =>
@@ -45,8 +31,9 @@ const columns = [
     }
 ];
 
-export const SellerProductsPage = (props) => {
+export const SellerProductsPage = () => {
     const [selectedSeller, setSelectedSeller] = React.useState(Sellers.sellers[0]);
+    const [selectionModel, setSelectionModel] = React.useState();
 
     const handleSellerClick = (event) => {
         setSelectedSeller(event.target.name);
@@ -67,7 +54,9 @@ export const SellerProductsPage = (props) => {
             ))
         );
     }
-    
+
+    const rows = FilterSellerProducts();
+
     return (
         <Grid container>
             <Grid item>
@@ -76,27 +65,44 @@ export const SellerProductsPage = (props) => {
             <Grid item className="sellerButtonsContainer">
                 <Link to="/seller/add-product-form" className="RoutingLink">
                     <Button className="sellerButton" variant="contained">
-                        Add product <AddIcon/>
+                        Add product <AddIcon />
                     </Button>
                 </Link>
                 <Link to="/seller/someId" className="RoutingLink">
                     <Button className="sellerButton" variant="contained" onClick={editProduct}>
-                        Edit product <EditIcon/>
+                        Edit product <EditIcon />
                     </Button>
                 </Link>
                 <Button className="sellerButton" variant="outlined" onClick={removeProduct}>
-                    Delete product <DeleteIcon/>
+                    Delete product <DeleteIcon />
                 </Button>
             </Grid>
             <Grid item sm={12} className="sellerTable">
-                <div style={{height: 400, width: '100%'}}>
-                    <div style={{display: 'flex', height: '100%'}}>
-                        <div style={{flexGrow: 1}}>
+                <div style={{ height: 400, width: '100%' }}>
+                    <div style={{ display: 'flex', height: '100%' }}>
+                        <div style={{ flexGrow: 1 }}>
                             <DataGrid
-                                rows={FilterSellerProducts()}
+                                rows={rows}
                                 columns={columns}
                                 pageSize={5}
                                 rowsPerPageOptions={[5]}
+                                checkboxSelection
+                                selectionModel={selectionModel}
+                                hideFooterSelectedRowCount
+                                onSelectionModelChange={(selection) => {
+                                    if (selection.length > 1) {
+                                        const selectionSet = new Set(selectionModel);
+                                        const result = selection.filter((s) => !selectionSet.has(s));
+                                        setSelectionModel(result);
+                                    } else {
+                                        setSelectionModel(selection);
+                                        const selectedSet = new Set(selectionModel);
+                                        const selectedRowData = rows.filter((row) =>
+                                            selectedSet.has(row.id)
+                                        );
+                                        console.log(selectedRowData[0].name); //get row id instead of 0 from filter
+                                    }
+                                }}
                             />
                         </div>
                     </div>
