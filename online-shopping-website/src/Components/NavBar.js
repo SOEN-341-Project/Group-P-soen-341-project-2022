1560aa4503a7b8ea58129323fbffd592db1ad2d4
emma-amar <74879773+emma-amar@users.noreply.github.com>
2022-02-21 20:14:55 -0500

    navbar profile card and other updates

--- online-shopping-website/src/Components/NavBar.js
+++ online-shopping-website/src/Components/NavBar.js
@@ -12,75 +12,23 @@ import { alpha, styled } from '@mui/material/styles';
 import Avatar from '@mui/material/Avatar';
 import { Container } from '@mui/material/Container';
 import { borderRight } from '@mui/system';
-import { Menu, MenuItem } from '@mui/material';
+import { FormControlLabel, FormGroup, Menu, MenuItem, Switch } from '@mui/material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import {Link} from "react-router-dom";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
-const Search = styled('div')(({theme}) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25)
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width:'auto',
-    },
-}));
-
-const SearchIconWrapper = styled('div')(({theme}) => ({
-    padding: theme.spacing(0,2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({theme}) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1,1,1,0),
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
-        },
-    },
-}));
-
-const links = ['BOBBLE','Menu','Cart'];
-
-//the below section is some preliminary stuff for the user profile
-// const profileLoggedIn = [maskEmail, 'Profile', 'Logout'];
-// const profile=['Login', 'Register'];
-
-// const maskEmail="";
-// const fakeEmail="jeffb@amazon.com";
-// const prefix = fakeEmail.substring(0,fakeEmail .lastIndexOf("@"));
-// const postfix = fakeEmail.substring(fakeEmail .lastIndexOf('@'));
-
-// for (var i=0; i<prefix.length; i++){
-//     if(i==0 || i==prefix.length-1){
-//         maskEmail=maskEmail+prefix[i].toString();
-//     }
-//     else{
-//         maskEmail = maskEmail + "*";
-//     }
-// }
-// maskEmail = maskEmail+postfix;
+const links = ['BOBBLE','Cart'];
 
 export default function NavBar() {
+    const [auth, setAuth] = React.useState(true);
+    const [seller, setSeller] = React.useState(false);
     const [anchorEl, setAnchorEl] = React.useState(null);
-    
+
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+        setSeller(event.target.checked);
+    };
+
     const handleOpenUserMenu = (event) => {
         setAnchorEl(event.currentTarget);
     };
@@ -88,11 +36,16 @@ export default function NavBar() {
     const handleCloseUserMenu = () => {
         setAnchorEl(null);
     };
+
+
     const profileId = 'navbar-account-profile';
+    const unProfileId = 'navbar-unaccount-profile';
+    const sellerId = 'navbar-seller-profile';
     const renderAccountProfileIcon = (
         <MenuItem onClick={handleOpenUserMenu}>
         </MenuItem>
     )
+
     return (
         <Box sx={{ flexGrow: 1}}>
         <AppBar 
@@ -109,22 +62,13 @@ export default function NavBar() {
                 >
                 {links[0]}
                 </Typography>
+              
                 <Button color='inherit'><Link to="/" className="RoutingLink">Products</Link></Button>
                 <Button color='inherit'><Link to="/sellerPage" className="RoutingLink">Temporary Seller</Link></Button>
                 <Button color='inherit'>{links[1]}</Button>
-                <Button color='inherit'>{links[2]}</Button>
-                {/* <Button color='inherit'>{links[3]}</Button>
-                <Button color='inherit'>{links[4]}</Button> */}
                 </Box>
-                <Search>
-                    <SearchIconWrapper>
-                        <SearchIcon />
-                    </SearchIconWrapper>
-                    <StyledInputBase
-                        placeholder = "Search..."
-                        inputProps={{ 'aria-label': 'search'}}
-                    />
-                </Search>
+                 {auth && !seller && (
+                    <div>
                 <IconButton
                     size="large"
                     edge="end"
@@ -136,9 +80,96 @@ export default function NavBar() {
                 >
               <AccountCircle />
             </IconButton>
-            </Toolbar>
-        </AppBar>
-        {renderAccountProfileIcon}
-        </Box>
+            <Menu
+                id='navbar-account-profile'
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleCloseUserMenu}
+            >
+                <MenuItem onClick={handleCloseUserMenu}>Profile</MenuItem>
+                <MenuItem onClick={handleCloseUserMenu}>My Account Info</MenuItem>
+            </Menu>
+         </div>
+        )}
+            {auth && seller &&(
+                        <div>
+                     <IconButton
+                        size="large"
+                        edge="end"
+                        aria-label="account of current user"
+                        aria-controls={sellerId}
+                        aria-haspopup="true"
+                        onClick={handleOpenUserMenu}
+                        color="inherit"
+                    >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+                id='navbar-seller-profile'
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleCloseUserMenu}
+            >
+                <MenuItem onClick={handleCloseUserMenu}>Profile</MenuItem>
+                <MenuItem onClick={handleCloseUserMenu}>My Account Info</MenuItem>
+                <MenuItem onClick={handleCloseUserMenu}>Manage Orders</MenuItem>
+                <MenuItem onClick={handleCloseUserMenu}>Manage Products</MenuItem>
+            </Menu>
+         </div>
+        )}
+            {!auth && !seller &&(
+                    <div>
+                <IconButton
+                    size="large"
+                    edge="end"
+                    aria-label="account of non member"
+                    aria-controls={unProfileId}
+                    aria-haspopup="true"
+                    onClick={handleOpenUserMenu}
+                    color="inherit"
+                >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+                id='navbar-unaccount-profile'
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleCloseUserMenu}
+            >
+                <MenuItem onClick={handleCloseUserMenu}>Sign Up!</MenuItem>
+            </Menu>
+         </div>
+        )}
+     </Toolbar>
+    </AppBar>
+    {renderAccountProfileIcon}
+    </Box>
     );
 }
