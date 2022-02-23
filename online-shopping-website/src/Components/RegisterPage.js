761a2a9b0f38ed2000dcb73da5d561a9e2286d4f
Jungsoo <jungsoo.lee4@gmail.com>
2022-02-23 18:28:07 -0500

    modified the password validation

--- online-shopping-website/src/Components/RegisterPage.js
+++ online-shopping-website/src/Components/RegisterPage.js
@@ -32,26 +32,6 @@ export const Register = () => {
     //state for profile components
     const [values, setValues] = useState(profileProperties);
 
-    //to handle the password input
-    const handlePassword = (e) => {
-        let checkPassword = '' + e.target.value + '';
-        checkPassword = checkPassword.replace(/\\/g, "\\\\");
-        checkPassword = checkPassword.replace(/\./g, "\\.");
-        checkPassword = checkPassword.replace(/\+/g, "\+.");
-        checkPassword = checkPassword.replace(/\*/g, "\\*");
-        checkPassword = checkPassword.replace(/\?/g, "\\?");
-        checkPassword = checkPassword.replace(/\^/g, "\\^");
-        checkPassword = checkPassword.replace(/\$/g, "\\$");
-        checkPassword = checkPassword.replace(/\(/g, "\\(");
-        checkPassword = checkPassword.replace(/\)/g, "\\)");
-        checkPassword = checkPassword.replace(/\[/g, "\\[");
-        checkPassword = checkPassword.replace(/\]/g, "\\]");
-        checkPassword = checkPassword.replace(/\{/g, "\\{");
-        checkPassword = checkPassword.replace(/\}/g, "\\}");
-        checkPassword = checkPassword.replace(/\|/g, "\\|");
-        setValues({ ...values, password: e.target.value, confPassword: checkPassword});
-    }
-
     //function executes when user selects seller or customer through radio buttons
     const handleChange = (event) => {
         setValues({ ...values, role: event.target.value })
@@ -66,8 +46,10 @@ export const Register = () => {
 
     //when user clicks submits form
     const processRegister = (event) => {
-        event.preventDefault();
-        outputProfile();
+        if(values.password !== values.confPassword) {
+            event.preventDefault();
+            outputProfile();
+        }
     }
 
     //to test the profile prop values
@@ -80,15 +62,15 @@ export const Register = () => {
     return (
         <div className='register-page'>
             {/* sign up form */}
-            <form onSubmit={processRegister} className='sign-up'>
+            <form onSubmit={processRegister} action="/" className='sign-up'>
                 <Stack spacing={2}>
                     <p className="Sign-Up-Text">Sign Up</p>
                     <TextField className='textfield-register' label="First name" variant="outlined" value={values.firstName} onChange={(e) => setValues({ ...values, firstName: e.target.value })} />
                     <TextField label="Last name" variant="outlined" value={values.lastName} onChange={(e) => setValues({ ...values, lastName: e.target.value })} />
                     <TextField required inputProps={{ pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]+$", title: "username@domain"}} label="Email" variant="outlined" value={values.email} onChange={(e) => setValues({ ...values, email: e.target.value })} />
                     <TextField label="Username" variant="outlined" value={values.username} onChange={(e) => setValues({ ...values, username: e.target.value })} />
-                    <TextField required type="password" inputProps={{ pattern: '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_=+|:;<>,.?/~(){}\\[\\]\\\\-]).{8,}$', title: 'Password must follow this format: - At least one digit - At least one lowercase character - At least one uppercase character - At least one special character' }} label="Password" variant="outlined" value={values.password} onChange={handlePassword} />
-                    <TextField required type="password" label="Confirm password" variant="outlined" inputProps={{ pattern: '^' + values.confPassword + '$', title: 'Passwords do not match' }} />
+                    <TextField required type="password" inputProps={{ pattern: '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_=+|:;<>,.?/~(){}\\[\\]\\\\-]).{8,}$', title: 'Password must follow this format: - At least one digit - At least one lowercase character - At least one uppercase character - At least one special character' }} label="Password" variant="outlined" value={values.password} onChange={(e) => setValues({...values, password: e.target.value})} />
+                    <TextField required type="password" label="Confirm password" variant="outlined" value={values.confPassword} onChange={(e) => setValues({ ...values, confPassword: e.target.value})} error={values.confPassword !== values.password} helperText={values.confPassword !== values.password ? "Passwords do not match" : ''} />
                     <br/><label>Shipping address:</label><br/>
                     <TextField required inputProps={{ pattern: '^[0-9]+, [A-Za-z0-9 -]+$', title: '"Street Number, Street Name" Valid Characters: A-Z, a-z, 0-9' }} label="Street" variant="outlined" value={values.street} onChange={(e) => setValues({ ...values, street: e.target.value})} />
                     <TextField required inputProps={{ pattern: '^[A-Za-z0-9 -]+$', title: 'Valid Characters: A-Z, a-z, 0-9 and hyphen' }} label="City" variant="outlined" value={values.city} onChange={(e) => setValues({ ...values, city: e.target.value})} />
