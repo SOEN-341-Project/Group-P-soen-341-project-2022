f743556cb159abaca01e57f330c5de37b7c68b30
KooshaG <75464454+KooshaG@users.noreply.github.com>
2022-03-09 11:35:22 -0500

    Refactored filter logic
    Co-authored-by: Adamo Orsini <adamo.orsini01@gmail.com>

--- online-shopping-website/frontend/src/App.js
+++ online-shopping-website/frontend/src/App.js
@@ -1,76 +1,43 @@
-import './App.css';
-import NavBar from './Components/NavBar';
-import {SellerProductsPage} from './Components/Seller/SellerProductsPage';
-import {SellerProductsForm} from './Components/Seller/SellerProductsForm';
-import { ProductPage } from './Components/ProductPage';
-import { ProductDetails } from './Components/ProductDetails';
-import { useState } from 'react';
-import Products from './TestValues.json';
-import {Register} from './Components/RegisterPage';
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import { SellerProductsPage } from "./Components/Seller/SellerProductsPage";
+import { SellerProductsForm } from "./Components/Seller/SellerProductsForm";
+import { ProductPage } from "./Components/ProductPage";
+import { ProductDetails } from "./Components/ProductDetails";
+import { useState } from "react";
+import Products from "./TestValues.json";
+import { Register } from "./Components/RegisterPage";
 import { BrowserRouter, Outlet, Routes, Route } from "react-router-dom";
 
 const Layout = () => {
-    return (
-        <>
-            <NavBar sx={<h1></h1>}/>
-            <div className="AppContent">
-                <Outlet/>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <NavBar sx={<h1></h1>} />
+      <div className="AppContent">
+        <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default function App() {
-    // Products that will render
-    const [products, setProducts] = useState(Products.products);
-
-    // Filters
-    let [filters] = useState(
-        {
-            searchQuery: '',
-            brands: [],
-            sellers: [],
-            lowestPrice: null,
-            highestPrice: null
-        }
-    );
-
-    const filterProducts = () => {
-        // Use RegEx for search query
-        /*
-            ^ = Match beginning of product name
-            i = Case insensitive
-        */
-        const query = new RegExp('^' + filters.searchQuery, 'i');
-
-        // Filter products according to search query, are within price range, and don't belong to brands or sellers arrays
-        setProducts(Products.products.filter(product => (
-            (product.name.split(' ').some(word => word.search(query) > -1) || product.name.search(query) > -1)
-            && (filters.lowestPrice ? (product.price >= filters.lowestPrice) : true)
-            && (filters.highestPrice ? (product.price <= filters.highestPrice) : true)
-            && !filters.brands.includes(product.brand)
-            && !filters.sellers.includes(product.seller)
-        )));
-    }
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout/>}>
-                    <Route index element={<ProductPage filterProducts={filterProducts} filters={filters}
-                           products={products}/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/seller/add-product-form" element={<SellerProductsForm/>}/>
-                    <Route path="/seller/:productId" element={<SellerProductsForm/>}/>
-                    <Route path="/seller" element={<SellerProductsPage products={products}/>}/>
-                    <Route path="/:productId/:productName" element={<ProductDetails/>}/>
-                    <Route path="*" element={<NoPage/>}/>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<ProductPage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/seller/add-product-form" element={<SellerProductsForm />} />
+          <Route path="/seller/:productId" element={<SellerProductsForm />} />
+          <Route path="/seller" element={<SellerProductsPage />} />
+          <Route path="/:productId/:productName" element={<ProductDetails />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 const NoPage = () => {
-    return <h1>404</h1>;
+  return <h1>404</h1>;
 };
--- online-shopping-website/frontend/src/Components/ProductPage.js
+++ online-shopping-website/frontend/src/Components/ProductPage.js
@@ -1,13 +1,44 @@
-import TestData from '../TestValues.json';
-import Grid from '@mui/material/Grid';
-import { ProductGrid } from './ProductGrid';
-import { SideNav } from './SideNav';
-import {SearchBar} from './SearchBar';
+import { useState } from "react";
+import TestData from "../TestValues.json";
+import Grid from "@mui/material/Grid";
+import { ProductGrid } from "./ProductGrid";
+import { SideNav } from "./SideNav";
+import { SearchBar } from "./SearchBar";
 
 // Encapsulates both SideNav and ProductGrid
-export const ProductPage = (props) => {
-  let filters = props.filters;
-  let filterProducts = props.filterProducts;
+export const ProductPage = () => {
+  // Products that will render
+  const [products, setProducts] = useState(TestData.products);
+
+  // Filters
+  let [filters] = useState({
+    searchQuery: "",
+    brands: [],
+    sellers: [],
+    lowestPrice: null,
+    highestPrice: null,
+  });
+
+  const filterProducts = () => {
+    // Use RegEx for search query
+    /*
+          ^ = Match beginning of product name
+          i = Case insensitive
+      */
+    const query = new RegExp("^" + filters.searchQuery, "i");
+
+    // Filter products according to search query, are within price range, and don't belong to brands or sellers arrays
+    setProducts(
+      TestData.products.filter(
+        (product) =>
+          (product.name.split(" ").some((word) => word.search(query) > -1) || product.name.search(query) > -1) &&
+          (filters.lowestPrice ? product.price >= filters.lowestPrice : true) &&
+          (filters.highestPrice ? product.price <= filters.highestPrice : true) &&
+          !filters.brands.includes(product.brand) &&
+          !filters.sellers.includes(product.seller)
+      )
+    );
+  };
 
   const onSliderChange = (priceRange) => {
     // Set price range from slider
@@ -16,40 +47,45 @@ export const ProductPage = (props) => {
 
     // Update product state with filters
     filterProducts();
-  }
+  };
 
   const onCheckboxChange = (filterType, filterName, isChecked) => {
     // Unchecked checkbox -> Add filter to filters state
     if (!isChecked) {
-      if (filterType === 'Brand') {
+      if (filterType === "Brand") {
         filters.brands.push(filterName);
-      }
-      else if (filterType === 'Seller') {
+      } else if (filterType === "Seller") {
         filters.sellers.push(filterName);
       }
     }
     // Checked checkbox -> Remove filter from filters state
     else {
-      if (filterType === 'Brand') {
+      if (filterType === "Brand") {
         filters.brands = filters.brands.filter((f) => f !== filterName);
-      }
-      else if (filterType === 'Seller') {
+      } else if (filterType === "Seller") {
         filters.sellers = filters.sellers.filter((f) => f !== filterName);
       }
     }
 
     // Update product state with filters
     filterProducts();
-  }
+  };
 
   return (
     <Grid container columnSpacing={4} rowSpacing={5}>
       <Grid item xs={12} md={3} lg={2}>
-        <SideNav brands={TestData} sellers={TestData} filterProducts={filterProducts} filters={filters} onCheckboxChange={onCheckboxChange} onSliderChange={onSliderChange} />
+        <SideNav
+          brands={TestData}
+          sellers={TestData}
+          filterProducts={filterProducts}
+          filters={filters}
+          onCheckboxChange={onCheckboxChange}
+          onSliderChange={onSliderChange}
+        />
       </Grid>
       <Grid item xs={12} md={9} lg={10}>
-        <ProductGrid products={props.products}/>
+        <ProductGrid products={products} />
       </Grid>
     </Grid>
   );
-}
+};
