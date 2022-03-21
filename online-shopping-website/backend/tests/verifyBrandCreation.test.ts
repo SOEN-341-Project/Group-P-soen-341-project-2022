8ba487d7450c61b26b37a1161acfee9692f969de
emma-amar <emma.h.amar@gmail.com>
2022-03-20 21:49:12 -0400

    created verifyBrandCreation tests

---
+++ online-shopping-website/backend/tests/verifyBrandCreation.test.ts
@@ -0,0 +1,35 @@
+import hasRequiredBrandCreationParams from "../src/helpers/verifyBrandCreation";
+
+const data = ["name", "description"];
+let requestBody: any = { name: 'name', description: 'description'};
+
+describe("Required Brand Creation Params Tests", () => {
+    test("No name", () => {
+        requestBody.name = undefined;
+        requestBody.description = data[1];
+
+        expect(hasRequiredBrandCreationParams(requestBody)).toEqual(false);
+    });
+
+    test("No description", () => {
+        requestBody.name = data[0];
+        requestBody.description = undefined;
+
+        expect(hasRequiredBrandCreationParams(requestBody)).toEqual(false);
+    });
+
+    test("Nothing at all", () => {
+        requestBody.name = undefined;
+        requestBody.description = undefined;
+
+        expect(hasRequiredBrandCreationParams(requestBody)).toEqual(false);
+    });
+
+    test("Good to create brand", () => {
+        requestBody.name = data[0];
+        requestBody.description = data[1];
+
+        expect(hasRequiredBrandCreationParams(requestBody)).toEqual(true);
+    });
+
+});
