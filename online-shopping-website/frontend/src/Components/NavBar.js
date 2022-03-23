5a68b218947cdc367e6e2917ddd49f6ae8d65012
Adamo O <adamo.orsini12@gmail.com>
2022-03-23 14:16:42 -0400

    Added basic set and delete user cookies in NavBar login and logout

--- online-shopping-website/frontend/src/Components/NavBar.js
+++ online-shopping-website/frontend/src/Components/NavBar.js
@@ -19,13 +19,15 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import TestData from '../TestValues.json';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
 export default function NavBar() {
     const [auth, setAuth] = React.useState(false);
     const [seller, setSeller] = React.useState(false);
     const [admin, setAdmin] = React.useState(false);
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const [cookies, setCookie, deleteCookie] = useCookies(['user', 'cart']);
 
     //initializing user credentials
     const [user, setUser] = React.useState({
@@ -47,6 +49,11 @@ export default function NavBar() {
             email: '',
             password: ''
         });
+
+        // Delete user and cart cookies
+        deleteCookie('user');
+        deleteCookie('cart');
+
         window.alert("Successfully logged out.");
     }
 
@@ -60,51 +67,23 @@ export default function NavBar() {
         setOpenLogin(false);
     };
 
-    const handleLoginSubmit = (event) => {
-        //Username and password are valid
-        if (TestData.users.filter(tempUser => tempUser.email === user.email && tempUser.password === user.password)[0]) {
-            //login as default (customer access)
+    const handleLoginSubmit = async(event) => {
+        event.preventDefault();
+
+        try {
+            const loginResponse = await axios.post(process.env.REACT_APP_DB_CONNECTION + "/api/users/signin", user);
+            console.log(loginResponse.data);
+
             setAuth(true);
-            console.log("valid user");
 
-            //User is a seller
-            if (TestData.users.filter(tempUser => tempUser.email === user.email && tempUser.role === "seller")[0]) {
-                //login as seller
-                setSeller(true);
-                console.log("user is a seller");
-            }
-            //User is an admin
-            else if (TestData.users.filter(tempUser => tempUser.email === user.email && tempUser.role === "admin")[0]) {
-                //login as admin
-                setAdmin(true);
-                console.log("user is an admin");
-            } else {
-                console.log("user is a customer");
-            }
-            //close login when correct info is entered
-            handleCloseLogin();
+            // TODO Check if seller or admin when user object returned
+            
+            setCookie('user', loginResponse.data);
         }
-        //Email and password don't match
-        else if (TestData.users.filter(tempUser => tempUser.email === user.email && tempUser.password !== user.password)[0]) {
-            console.log("email and password don't match");
-            window.alert("Incorrect Password");
-
-            //clear form
-            event.preventDefault();
-        }
-        //email is not in database
-        else if (!(TestData.users.filter(tempUser => tempUser.email === user.email)[0])) {
-            console.log("invalid email");
-            window.alert("There is no account associated with this email.");
-
-            //clear form
-            event.preventDefault();
-        }
-        // email and password are not in database (never reaching?)
-        else {
-            console.log("incorrect email or password");
-            window.alert("Incorrect login credentials. Verify that email and password have been entered correctly.");
-            event.preventDefault();
+        catch (err) {
+            window.alert(
+                err.response.data.error + ".\n" + 
+                (err.response.data.message ? err.response.data.message + "." : ""));
         }
     };
 
@@ -310,7 +289,7 @@ export default function NavBar() {
             <div>
                 <Dialog open={openLogin} onClose={handleCloseLogin}>
                     <DialogTitle style={{paddingBottom: 0}}>Login</DialogTitle>
-                    <form onSubmit={handleLoginSubmit} action="/">
+                    <form onSubmit={handleLoginSubmit} >
                         <DialogContent>
 
                             <TextField
