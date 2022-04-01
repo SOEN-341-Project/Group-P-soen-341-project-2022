294a2e268418ec72fbc28010036f319b1047a529
Alexandra Spyridakos <71853151+aspyridakos@users.noreply.github.com>
2022-04-01 09:52:28 -0400

    hiding save button when not editing profile

--- online-shopping-website/frontend/src/Components/Profile/ProfilePage.js
+++ online-shopping-website/frontend/src/Components/Profile/ProfilePage.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import EditIcon from '@mui/icons-material/Edit';
-import {useCookies} from 'react-cookie';
+import { useCookies } from 'react-cookie';
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import CloseIcon from '@mui/icons-material/Close';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 export const ProfilePage = () => {
@@ -33,94 +33,97 @@ export const ProfilePage = () => {
         <div>
             <Link to="/" className='RoutingLink'>
                 <Button variant="text" className="ProductsBackButton">
-                    <ArrowBackIosNewIcon/><h4>Return to products</h4>
+                    <ArrowBackIosNewIcon /><h4>Return to products</h4>
                 </Button>
             </Link>
             <Grid item container xs={12} className='ProfileContainer'>
                 <Grid item xs={8}>
                     <h1>My Profile</h1>
                 </Grid>
-                <Grid item xs={4} sx={{paddingTop: '1rem'}}>
+                <Grid item xs={4} sx={{ paddingTop: '1rem' }}>
                     <Button onClick={() => {
                         setEditable(!editable)
-                    }} style={{float: 'right'}} variant="contained"
-                            className="GreenButtonContained EditProfileButtonLarge">
-                        {editable ? (<CloseIcon/>) : (<EditIcon/>)}
+                    }} style={{ float: 'right' }} variant="contained"
+                        className="GreenButtonContained EditProfileButtonLarge">
+                        {editable ? (<CloseIcon />) : (<EditIcon />)}
                         {editable ? 'Cancel Edit' : 'Edit Profile'}
                     </Button>
                     <Button onClick={() => {
                         setEditable(!editable)
-                    }} style={{float: 'right'}} variant="contained"
-                            className="GreenButtonContained EditProfileButtonSmall">
-                        {editable ? (<CloseIcon/>) : (<EditIcon/>)}
+                    }} style={{ float: 'right' }} variant="contained"
+                        className="GreenButtonContained EditProfileButtonSmall">
+                        {editable ? (<CloseIcon />) : (<EditIcon />)}
                     </Button>
                 </Grid>
                 <Grid item xs={12} className='ProfileInfoContainer'>
                     <form onSubmit={handleSubmit}>
                         {/*First name*/}
-                        <h2 className="ProfileInfoHeader" style={{marginTop: '1rem'}}>First name</h2>
+                        <h2 className="ProfileInfoHeader" style={{ marginTop: '1rem' }}>First name</h2>
                         <TextField className="ProfileTextField"
-                                   placeholder="First name"
-                                   variant="outlined"
-                                   disabled={!editable}
-                                   inputProps={{
-                                       pattern: "([A-ZÀ-ÿ][-,a-zà-ÿ. ']+[ ]*)+",
-                                       title: 'Name must be alphabetical characters only.'
-                                   }}
-                                   value={userData.firstName ? userData.firstName : ''}
-                                   onChange={(e) => setUserData({...userData, firstName: e.target.value})}/>
+                            placeholder="First name"
+                            variant="outlined"
+                            disabled={!editable}
+                            inputProps={{
+                                pattern: "([A-ZÀ-ÿ][-,a-zà-ÿ. ']+[ ]*)+",
+                                title: 'Name must be alphabetical characters only.'
+                            }}
+                            value={userData.firstName ? userData.firstName : ''}
+                            onChange={(e) => setUserData({ ...userData, firstName: e.target.value })} />
 
                         {/*Last name*/}
                         <h2 className="ProfileInfoHeader">Last name</h2>
                         <TextField className="ProfileTextField"
-                                   placeholder="Last name"
-                                   variant="outlined"
-                                   disabled={!editable}
-                                   inputProps={{
-                                       pattern: "([A-ZÀ-ÿ][-,a-zà-ÿ. ']+[ ]*)+",
-                                       title: 'Name must be alphabetical characters only.'
-                                   }}
-                                   value={userData.lastName ? userData.lastName : ''}
-                                   onChange={(e) => setUserData({...userData, lastName: e.target.value})}/>
+                            placeholder="Last name"
+                            variant="outlined"
+                            disabled={!editable}
+                            inputProps={{
+                                pattern: "([A-ZÀ-ÿ][-,a-zà-ÿ. ']+[ ]*)+",
+                                title: 'Name must be alphabetical characters only.'
+                            }}
+                            value={userData.lastName ? userData.lastName : ''}
+                            onChange={(e) => setUserData({ ...userData, lastName: e.target.value })} />
 
                         {/*Email*/}
                         <h2 className="ProfileInfoHeader">Email</h2>
-                        <TextField sx={{paddingBottom: '1.5rem'}}
-                                   className="ProfileTextField"
-                                   disabled={!editable}
-                                   required
-                                   inputProps={{
-                                       pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]+$",
-                                       title: "username@domain"
-                                   }}
-                                   placeholder="someone@domain.com" variant="outlined" value={userData.email}
-                                   onChange={(e) => setUserData({...userData, email: e.target.value})}/>
+                        <TextField sx={{ paddingBottom: '1.5rem' }}
+                            className="ProfileTextField"
+                            disabled={!editable}
+                            required
+                            inputProps={{
+                                pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]+$",
+                                title: "username@domain"
+                            }}
+                            placeholder="someone@domain.com" variant="outlined" value={userData.email}
+                            onChange={(e) => setUserData({ ...userData, email: e.target.value })} />
 
                         {/*Username*/}
                         <h2 className="ProfileInfoHeader">Username</h2>
-                        <TextField sx={{paddingBottom: '1.5rem'}}
-                                   className="ProfileTextField"
-                                   variant="outlined"
-                                   placeholder="username"
-                                   disabled={!editable}
-                                   value={userData.username ? userData.username : ''}
-                                   onChange={(e) => setUserData({...userData, username: e.target.value})}/>
+                        <TextField sx={{ paddingBottom: '1.5rem' }}
+                            className="ProfileTextField"
+                            variant="outlined"
+                            placeholder="username"
+                            disabled={!editable}
+                            value={userData.username ? userData.username : ''}
+                            onChange={(e) => setUserData({ ...userData, username: e.target.value })} />
 
                         {/*Address*/}
                         <h2 className="ProfileInfoHeader">Shipping Address</h2>
                         <TextField className="ProfileTextField"
-                                   variant="outlined"
-                                   required multiline
-                                   placeholder="address"
-                                   value={userData.address1}
-                                   disabled={!editable}
-                                   onChange={(e) => setUserData({...userData, address1: e.target.value})}/>
+                            style={{ marginBottom: "1rem" }}
+                            variant="outlined"
+                            required multiline
+                            placeholder="address"
+                            value={userData.address1}
+                            disabled={!editable}
+                            onChange={(e) => setUserData({ ...userData, address1: e.target.value })} />
 
                         {/*Save button*/}
-                        <div style={{textAlign: 'center', margin: '2rem 0 1rem 0'}}>
-                            <Button type="submit" value="Sign Up" variant="contained" className='GreenButtonContained'
-                                    sx={{maxWidth: '10rem'}}>Save changes</Button>
-                        </div>
+                        {editable && (
+                            <div style={{ textAlign: 'center', margin: '2rem 0 1rem 0' }}>
+                                <Button type="submit" value="Sign Up" variant="contained" className='GreenButtonContained'
+                                    sx={{ maxWidth: '10rem' }}>Save changes</Button>
+                            </div>
+                        )}
                     </form>
                 </Grid>
             </Grid>
