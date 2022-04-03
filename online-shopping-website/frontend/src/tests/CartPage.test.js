07d3af95b387e79f852618a97cea92c7313a1d11
Adamo Orsini <adamo.orsini12@gmail.com>
2022-04-03 11:59:26 -0400

    Added Cart Page test doc

---
+++ online-shopping-website/frontend/src/tests/CartPage.test.js
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+describe('Cart Page Tests', () => {
+  it('true all the time', () => {
+    expect(true).toBe(true);
+  });
+
+  it('page renders', () => {
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByText('My Shopping Cart')).toBeInTheDocument();
+  })
+});
