2b768ed4627043f0bc6b783911d89d0e60aa06f9
Adamo Orsini <adamo.orsini12@gmail.com>
2022-04-03 20:22:00 -0400

    Generalized seller form logic, made eslint warnings instead of errors

--- online-shopping-website/frontend/src/App.js
+++ online-shopping-website/frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import NavBar from './Components/NavBar';
 import { SellerProductsPage } from './Components/Seller/SellerProductsPage';
-import { ModifyProductForm, AddNewProductForm } from './Components/Seller/SellerProductsForm';
+import { ModifyProductForm, AddNewProductForm, SellerForm } from './Components/Seller/SellerProductsForm';
 import { ProductPage } from './Components/Products/ProductPage';
 import { ProductDetails } from './Components/Products/ProductDetails';
 import { CartPage } from './Components/CartPage';
@@ -35,8 +35,8 @@ export default function App() {
                     <Route index element={<ProductPage />}/>
                     <Route path="/register" element={<Register/>}/>
                     <Route path="/my-shopping-cart" element={<CartPage />}/>
-                    <Route path="/seller/add-product-form" element={<AddNewProductForm />}/>
-                    <Route path="/seller/:productId" element={<ModifyProductForm />}/>
+                    <Route path="/seller/add-product-form" element={<SellerForm />}/>
+                    <Route path="/seller/:productId" element={<SellerForm />}/>
                     <Route path="/seller" element={<SellerProductsPage />}/>
                     <Route path="/:productId/:productName" element={<ProductDetails />}/>
                     <Route path="/profile" element={<ProfilePage/>}/>
