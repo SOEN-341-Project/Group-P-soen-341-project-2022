934d55f76ba9c6058488520a62668c000379324c
Alexandra <aspyridakos@gmail.com>
2022-04-10 20:56:00 -0400

    removed code smells

--- online-shopping-website/frontend/src/Components/Browse/Products/Slideshow.js
+++ online-shopping-website/frontend/src/Components/Browse/Products/Slideshow.js
@@ -1,46 +1,42 @@
 import * as React from 'react';
 import 'react-slideshow-image/dist/styles.css';
-import { Slide } from "react-slideshow-image";
-import { Link } from "react-router-dom";
+import {Slide} from "react-slideshow-image";
+import {Link} from "react-router-dom";
 
 //for every promoted product, add it in the slideshow
 const ProductShow = (props) => {
-  console.log(props);
-  return(
-    <Link to={{
-      pathname: `/${props.product.id}/${props.product.name}`,
-      params: { props }
-    }} className="RoutingLink">
-      <div className="each-slide">
-        <div>
-          <div className="CenteringProductDetails">
+    return (
+        <Link to={{
+            pathname: `/${props.product.id}/${props.product.name}`,
+            params: {props}
+        }} className="RoutingLink">
             <div className="ProductDetailsShow">
-              <img className="ProductImageShow" src={props.product.picture} alt={props.product.name} />
-              <div className="ProductInfoSlide">
-                <h3>{props.product.name}</h3>
-                <p>Brand: {props.product.brand.name}</p>
-                <p>Sold by: {props.product.seller.sellerName}</p>
-              </div>
+                <img className="ProductImageShow" src={props.product.picture} alt={props.product.name}/>
+                <div className="TextGreen">
+                    <h3 style={{marginBottom: "0"}}>{props.product.name}</h3>
+                    <p style={{margin: "0.5rem"}}>
+                        Brand: {props.product.brand.name}
+                        <br/>
+                        Sold by: {props.product.seller.sellerName}
+                    </p>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </Link>
-  )
+        </Link>
+    )
 }
 
 //boilerplate for slideshow
 export const Slideshow = (props) => {
-  return (
-    <div className="SlideshowPadding">
-      <h1 className="PromotedItems">SPECIAL!</h1>
-      <Slide easing="ease">
-        {props.products.map(product => {
-          return (
-            <ProductShow key={product.id} product={product} />
-          );
-        })}
-      </Slide>
-    </div>
-  )
+    return (
+        <div className="SlideshowPadding">
+            <h1 style={{margin: 0}}>Promotions</h1>
+            <Slide easing="ease">
+                {props.products.map(product => {
+                    return (
+                        <ProductShow key={product.id} product={product}/>
+                    );
+                })}
+            </Slide>
+        </div>
+    )
 }
